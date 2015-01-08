98e72a566eaf6aa7bd7697458b8bb55ab2bb1c3a
simsun <sunsj1231@gmail.com>
2015-01-08 15:31:33 +0800

    implement method inheritance

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = Class
 
-function Class(_this) {
+function Class(_this, _super) {
   var _this_ctor
   if (_this && _this.initialize && typeof(_this.initialize) == 'function') {
     _this_ctor = _this.initialize;
@@ -8,7 +8,27 @@ function Class(_this) {
   } else {
     _this_ctor = function() {};
   }
-
-  _this_ctor.prototype = _this
+  __extends(_this_ctor, _super)
+  for (var v in _this) {
+    if (typeof(_this[v]) == "function") {
+      _this_ctor.prototype[v] = _this[v]
+    }
+  }
   return _this_ctor
 }
+
+var __extends = function(child, parent) {
+  // Copy "class properties" from parent to child
+  if (!parent) return;
+  for (var key in parent) {
+    child[key] = parent[key];
+  }
+
+  function ctor() {
+    this.constructor = child;
+  }
+
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  return child;
+};
